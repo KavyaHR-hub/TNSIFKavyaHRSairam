3ec31efa7e975d51de0009cf510e5cc19b819ef3
user-pc <user-pc@DESKTOP-KVKAJOH>
2025-11-22 15:51:51 +0530

    Updated code

---
+++ src/SuperKeyword/Demo.java
@@ -0,0 +1,18 @@
+package SuperKeyword;
+class Student{
+	
+}
+class R15 extends Vehicle{
+	
+}
+public class Demo {
+	public static void main(String[] args) {
+		Demo d=new Demo();
+	    R15 obj=new R15();
+	    Student s=new Student();
+	    System.out.println(s instanceof Student);
+		//System.out.println(s instanceof Student);
+		//System.out.println(d instanceof Demo);
+		}
+
+}
